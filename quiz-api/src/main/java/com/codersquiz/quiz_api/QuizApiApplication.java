14b0adf8c205e5d5a08584449223f78ffdc8743f
Hector Trillo <htrillo95@gmail.com>
2024-08-19 21:44:18 -0400

    first commit

--- quiz-api/src/main/java/com/codersquiz/quiz_api/QuizApiApplication.java
+++ quiz-api/src/main/java/com/codersquiz/quiz_api/QuizApiApplication.java
@@ -21,7 +21,7 @@ public class QuizApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
+						.allowedOrigins("http://localhost:3000","https://zingy-elf-444a3a.netlify.app")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
